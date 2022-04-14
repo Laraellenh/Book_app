d0895216a9a240009d9a781182396e13ae4fa96d
Lara <laraellenh@gmail.com>
2022-04-14 10:11:13 -0400

    fixed buttons

--- client/src/Components/OneBook.js
+++ client/src/Components/OneBook.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function OneBook({b}) {
+
+function OneBook({b, handleClick}) {
+    
+    
     console.log(b)
+    // b.map(bookObj=>console.log(bookObj)) if i want to show details of each book beyond title i can map the book objects and add the details as props
+    
+    
+
   return (
     <div>
-        <h3>{b.title}</h3>
-        <h4></h4>
+        <h4>{b.title}
+            <button 
+            onClick={()=>handleClick(b)}
+            >
+                Add to Your Favorites List
+            </button>
+        </h4>
+        
     </div>
   )
 }
